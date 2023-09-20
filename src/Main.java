919561b256e6ea4848146855805396546c73721f
Makhintosh <igoruszat@wp.pl>
2023-09-20 13:29:21 +0200

    Dodanie klasy Address

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Address {
+    private String ulica;
+    private String miasto;
+    private String kodPocztowy;
+
+    public Address(String ulica, String miasto, String kodPocztowy){
+        this.ulica = ulica;
+        this.miasto = miasto;
+        this.kodPocztowy = kodPocztowy;
+    }
+    public String getUlica(){
+        return ulica;
+    }
+    public void setUlica(String ulica){
+        this.ulica = ulica;
+    }
+
+    public String getMiasto(){
+        return miasto;
+    }
+    public void setMiasto(String miasto) {
+        this.miasto = miasto;
+    }
+
+    public String getKodPocztowy(){
+        return kodPocztowy;
+    }
+    public void setKodPocztowy(String kodPocztowy) {
+        this.kodPocztowy = kodPocztowy;
     }
 }
