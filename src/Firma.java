b04483cb343b2bd4cce804bf8930445cda48622b
Makhintosh <igoruszat@gmail.com>
2023-09-25 13:50:56 +0200

    Added firma and print in main

--- src/Firma.java
+++ src/Firma.java
@@ -1,7 +1,37 @@
 public class Firma {
-    private String NazwaFirmy;
+    private String nazwaFirmy;
 
-    private String Przemysl;
+    private String przemysl;
 
-    private String
+    private Adres siedziba;
+
+    public Firma(String nazwaFirmy, String przemysl, Adres siedziba) {
+        this.nazwaFirmy = nazwaFirmy;
+        this.przemysl = przemysl;
+        this.siedziba = siedziba;
+    }
+
+    public String getNazwaFirmy() {
+        return nazwaFirmy;
+    }
+
+    public void setNazwaFirmy(String nazwaFirmy) {
+        this.nazwaFirmy = nazwaFirmy;
+    }
+
+    public String getPrzemysl() {
+        return przemysl;
+    }
+
+    public void setPrzemysl(String przemysl) {
+        this.przemysl = przemysl;
+    }
+
+    public Adres getSiedziba() {
+        return siedziba;
+    }
+
+    public void setSiedziba(Adres siedziba) {
+        this.siedziba = siedziba;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,12 @@ public class Main {
         Ludz ludz1 = new Ludz("Igor", "Szatkowski", 18, adres1);
         Ludz ludz2 = new Ludz("Kacper", "Psikuta", 19, adres2);
         Ludz ludz3 = new Ludz("Patryk", "Malomam", 21, adres3);
+
+        Firma firma1 = new Firma("Zaklad pogrzebowy: zyc nie umierac", "Grabazniczy", adres1);
+
+        System.out.println("Pelne imie: " + ludz1.getCaleImie());
+        System.out.println("Nazwa firmy to: " + firma1.getNazwaFirmy());
+        ludz2.setWiek(69);
+        System.out.println("Zaktualizowano wiek: " + ludz2.getWiek());
     }
 }
